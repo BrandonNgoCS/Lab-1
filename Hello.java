d7d9792f4da57dc3e2cfb29489f74939f31d0854
BrandonNgoCS <78112857+BrandonNgoCS@users.noreply.github.com>
2021-01-27 16:13:59 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/******************************
+ * Hello
+ ******************************
+ * Brandon Ngo
+ * 1/27/2021
+ * 255 003
+ *****************************/
+ 
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Brandon");
+		System.out.println("Hello");
+		System.out.println("Lab #1");
+		System.out.println("1/27/2021");
+		System.out.println("255 003");
+		System.out.println();
+		}
+	}
